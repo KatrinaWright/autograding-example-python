84c1ac7fdc7472ad403d90574ff8079457c03ce2
Emily M. Hill <emhill@drew.edu>
2021-01-13 10:39:47 -0500

    Updated tests

--- hello_test.py
+++ hello_test.py
@@ -17,9 +17,16 @@ def test_hello_pass():
 def test_squared_sum_empty():
     assert hello.squared_sum( [] ) == 0
 
-def test_squared_sum():
-    assert hello.squared_sum([5, -2, 3]) == 38
+def test_squared_sum1():
+    assert hello.squared_sum([4]) == 16
+
+def test_squared_sum2():
     assert hello.squared_sum([-3, 4]) == 25
+
+def test_squared_sum3():
+    assert hello.squared_sum([5, -2, 3]) == 38
+
+def test_squared_sum4():
     assert hello.squared_sum([7, -1, 15, 0]) == 275
 
 # 5 Tests
@@ -28,6 +35,12 @@ def test_mix_empty():
 
 def test_mix():
     assert hello.mix("hello", "there") == "htehlelroe"
+
+def test_mix_4_4():
     assert hello.mix("1234", "abcd") == "1a2b3c4d"
+
+def test_mix_2_4():
     assert hello.mix("12", "abcd") == "1a2bcd"
+
+def test_mix_4_2():
     assert hello.mix("1234", "ab") == "1a2b34"
